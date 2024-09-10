859c2f0c572d22010d351b67cfd32a798daadb13
Nick-Dvesty <Nick-Dvesty@users.noreply.github.com>
2024-09-10 16:41:32 +0000

    Deploying to gh-pages from @ Nick-Dvesty/syspro_oop@2ffe4b045848392c7b7a546300bbf7a88969215f 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.syspro.Muller","l":"Heap"},{"p":"ru.nsu.syspro.Muller","l":"HeapSort"},{"p":"ru.nsu.syspro.Muller","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.syspro.muller","l":"Heap"},{"p":"ru.nsu.syspro.muller","l":"HeapSort"},{"p":"ru.nsu.syspro.muller","l":"Main"}]
