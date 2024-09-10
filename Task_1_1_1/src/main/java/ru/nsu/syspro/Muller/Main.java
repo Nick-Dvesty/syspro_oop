843e66e84b8e2ec128e95f40418bb55918d3c5c7
Nick-Dvesty <genmeln05@gmail.com>
2024-09-10 11:28:04 +0700

    task-1-1-1

---
+++ Task_1_1_1/src/main/java/ru/nsu/syspro/Muller/Main.java
@@ -0,0 +1,10 @@
+package ru.nsu.syspro.Muller;
+
+import ru.nsu.syspro.Muller.Heap;
+import ru.nsu.syspro.Muller.HeapSort;
+
+public class Main {
+    public static void main(String[] args) {;
+
+    }
+}
