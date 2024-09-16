363b5b33370ce5c5a3b31615b3e6aaaaf10ced61
Nick-Dvesty <genmeln05@gmail.com>
2024-09-17 03:28:54 +0700

    add class Gamer

---
+++ Task_1_2_1/src/main/java/ru/nsu/syspro/Muller/Gamer.java
@@ -0,0 +1,55 @@
+package ru.nsu.syspro.Muller;
+
+import java.util.ArrayList;
+
+
+public class Gamer {
+    private final ArrayList<Card> cards;
+
+    public Gamer() {
+        cards = new ArrayList<>();
+    }
+    public String PrintCards() {
+        StringBuilder printLine = new StringBuilder();
+        boolean HaveClosed = false;
+        printLine.append("[ ");
+        for (int i = 0; i < cards.size(); i++) {
+            printLine.append(cards.get(i).PrintText());
+            if (i != cards.size() - 1) printLine.append(", ");
+        }
+        printLine.append(" ]");
+        if (!HaveClosed) printLine.append(" => ").append(GetSum());
+        return printLine.toString();
+    }
+    public Card TakeCard(DeskCard deskCard, boolean closed) {
+        cards.add(deskCard.GetOneCard());
+        int sum = GetSum();
+        if (sum > 21) repairDesk();
+        return cards.getLast();
+    }
+    public void OpenCards(){
+        for (Card card : cards) {
+            if (card.IsClosed())card.Open();
+        }
+    }
+    public int GetSum(){
+        int sum = 0;
+        for (Card card : cards) {
+            sum += card.getCost();
+        }
+        return sum;
+    }
+
+    private void repairDesk() {
+        int sum = GetSum();
+        if (sum > 21) {
+            for (int i = cards.size() - 1; i != 0 && sum > 21; i--) {
+                if (cards.get(i).cost == 11) {
+                    cards.set(i, new Card(1, cards.get(i).label));
+                    sum -= 10;
+                }
+            }
+        }
+    }
+
+}
