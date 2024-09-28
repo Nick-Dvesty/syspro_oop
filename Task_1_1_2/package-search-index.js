e64b874a295519ccb0c753d537aab1a5f2149383
Nick-Dvesty <Nick-Dvesty@users.noreply.github.com>
2024-09-28 20:39:39 +0000

    Deploying to gh-pages from @ Nick-Dvesty/syspro_oop@043751e7b13fea4e0013922c2e38a3d70248a420 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.syspro.Muller"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.syspro.muller"}];updateSearchResults();
