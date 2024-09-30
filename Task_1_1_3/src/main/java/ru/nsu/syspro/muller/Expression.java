3394b267953dcc29857e98121d3aade013ed2d9e
Nick-Dvesty <genmeln05@gmail.com>
2024-09-30 13:14:57 +0700

    comments expression

--- Task_1_1_3/src/main/java/ru/nsu/syspro/muller/Expression.java
+++ Task_1_1_3/src/main/java/ru/nsu/syspro/muller/Expression.java
@@ -1,8 +1,33 @@
 package ru.nsu.syspro.muller;
 
 public abstract class Expression {
+
+    /**
+     * Convert expression in string.
+     *
+     * @return expression string
+     */
     public abstract String print ();
+
+    /**
+     * return value expression.
+     *
+     * @param variables string with values variables
+     * @return value expression
+     */
     public abstract int substitution (String variables);
+
+    /**
+     * differentiation expression.
+     *
+     * @return differentiated expression
+     */
     public abstract Expression dif();
+
+    /**
+     * simplify expression.
+     *
+     * @return simpled expression
+     */
     public abstract Expression simple ();
 }
