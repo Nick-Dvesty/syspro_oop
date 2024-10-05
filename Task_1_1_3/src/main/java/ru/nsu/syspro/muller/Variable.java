cad29411511cb12f4afcef0cced79e684bc9ae6d
Nick-Dvesty <genmeln05@gmail.com>
2024-10-06 00:19:36 +0700

    fix javadoc2

---
+++ Task_1_1_3/src/main/java/ru/nsu/syspro/muller/Variable.java
@@ -0,0 +1,59 @@
+package ru.nsu.syspro.muller;
+
+
+/**
+ * class variables.
+ */
+public class Variable extends Expression {
+    private final String name;
+
+    /**
+     * default constructor for string
+     * @param name name variable
+     */
+    public Variable(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < name.length(); i++) {
+            if ((name.charAt(i) > 'Z' || name.charAt(i) < 'A')
+                && (name.charAt(i) > 'z' || name.charAt(i) < 'a')){
+                throw new IllegalArgumentException();
+            }
+        }
+        this.name = name;
+    }
+
+    @Override
+    public String print() {
+        return name;
+    }
+
+    @Override
+    public double eval(String variables) {
+        int answer = 0;
+        if (!variables.contains(name)) throw new IllegalArgumentException();
+        for (int i = variables.indexOf(name) + name.length() + 3; i < variables.length(); i++) {
+            if (variables.charAt(i) == ';') {
+                break;
+            }
+            if (variables.charAt(i) >= '0' && variables.charAt(i) <= '9') {
+                answer = (answer * 10 + (variables.charAt(i) - '0'));
+                continue;
+            }
+            throw new IllegalArgumentException();
+        }
+        return answer;
+    }
+
+    @Override
+    public Expression derivative(String variables) {
+        return name.equals(variables) ? new Number("1") : new Number("0");
+    }
+
+    @Override
+    public Expression simple() {
+        return new Variable(name);
+    }
+
+}
